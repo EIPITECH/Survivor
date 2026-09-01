d125a9c3f845a6269dbdc5a4c7cedf50ef22629c
eiffis <enzo.iffis@epitech.eu>
2026-09-02 01:25:33 +0200

    feat(backend/jobs): import type orm and auth module for the job module

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Job } from './entities/job.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Job]), AuthModule],
   controllers: [JobsController],
   providers: [JobsService],
 })
