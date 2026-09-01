33f547cc6e22d03be63a4752e7fd060f7f965ded
eiffis <enzo.iffis@epitech.eu>
2026-09-01 14:55:49 +0200

    fix(backend/auth): fix auth module passport import with passport.register method

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,16 +5,18 @@ import { UsersModule } from '../users/users.module.js';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants.js';
+import { AuthController } from './auth.controller.js';
 
 @Module({
   imports: [
     UsersModule,
-    PassportModule,
+    PassportModule.register({ session: false }),
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
     }),
   ],
+  controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService],
 })
