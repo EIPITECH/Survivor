a594b2f7cc8529f705fba3bf1932fe5104837e0d
eiffis <enzo.iffis@epitech.eu>
2026-09-01 23:35:49 +0200

    feat(backend/jobs): import type orm module to jobs module

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [JobsController],
