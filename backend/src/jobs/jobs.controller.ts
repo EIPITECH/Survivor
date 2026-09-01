d05d838b72937e289e15e007a98ecbb0ed6e3acf
eiffis <enzo.iffis@epitech.eu>
2026-09-01 23:51:13 +0200

    feat(backend/jobs): jobs controller now use jwt strategy, an employer can't create a offer if he's not authenticated

--- backend/src/jobs/jobs.controller.ts
+++ backend/src/jobs/jobs.controller.ts
@@ -1,34 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/create-job.dto';
-import { UpdateJobDto } from './dto/update-job.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('jobs')
 export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @Post()
+  @ApiOperation({summary: 'Créer un nouvelle offre de job (authentification requise)'})
   create(@Body() createJobDto: CreateJobDto) {
     return this.jobsService.create(createJobDto);
   }
 
   @Get()
+  @ApiOperation({summary: 'Récupère toutes les offres de job (authentification non requise'})
   findAll() {
     return this.jobsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.jobsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
-    return this.jobsService.update(+id, updateJobDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.jobsService.remove(+id);
   }
 }
