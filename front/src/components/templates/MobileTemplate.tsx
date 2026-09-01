225bb4b8ab25983e94c247dd80d47547d9e7bb32
eiffis <enzo.iffis@epitech.eu>
2026-09-01 22:19:14 +0200

    Revert "feat(frontend): first version of frontend"
    This reverts commit 6cb495a832e2c04049ed3c3740a58f5c339f1b7e, reversing
    changes made to 8eff83345e808e8ef5f2c671452079ac6914bfa8.

--- front/src/components/templates/MobileTemplate.tsx
+++
@@ -1,9 +0,0 @@
-function MobileTemplate() {
-    return (
-        <div>
-            <p> coucou mobile </p>
-        </div>
-    )
-}
-
-export default MobileTemplate;
--- front/src/components/toggleSwitchLoc.tsx
+++
@@ -1,8 +0,0 @@
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-
-export default function SwitchLocation() {
-  return (
-        <FormControlLabel control={<Switch defaultChecked />} label="Location" />  
-    );
-}
