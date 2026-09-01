bd46afd4943455c7019295fdcf1ca6ccb6127a39
eiffis <enzo.iffis@epitech.eu>
2026-09-01 23:44:54 +0200

    feat(backend/auth): jwt-auth guard and jwt strategy added to protect the endpoints which needs an authenticated account

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants.js';
 import { AuthController } from './auth.controller.js';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { AuthController } from './auth.controller.js';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
