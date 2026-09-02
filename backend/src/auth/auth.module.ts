a3b3ff6ecc383b1a209de5e95cc22d98f3316146
eiffis <enzo.iffis@epitech.eu>
2026-09-02 23:46:42 +0200

    fix(backend/auth): remove .js extension for all the importations

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service.js';
-import { LocalStrategy } from './local.strategy.js';
-import { UsersModule } from '../users/users.module.js';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants.js';
-import { AuthController } from './auth.controller.js';
+import { jwtConstants } from './constants';
+import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
