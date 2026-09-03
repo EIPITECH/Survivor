3c95db8a65d081d1419a08803b00ff1b2143c0fc
eiffis <enzo.iffis@epitech.eu>
2026-09-04 00:05:24 +0200

    fix(backend/users): import passport module in users.module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import { PassportModule } from '@nestjs/passport';
 import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), PassportModule.register({ session: false }),],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
