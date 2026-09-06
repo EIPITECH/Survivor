dd4e12ba0f6927179a3c51c4444f3e3df133fe9c
eiffis <enzo.iffis@epitech.eu>
2026-09-06 19:25:55 +0200

    feat(backend/seekers): implementation of dto for seekers

--- backend/src/users/seekers/dto/create-seeker.dto.ts
+++ backend/src/users/seekers/dto/create-seeker.dto.ts
@@ -1,1 +1,25 @@
-export class CreateSeekerDto {}
+import {IsEmail, IsString, IsNotEmpty, MinLength, IsEnum, minLength} from 'class-validator'; 
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreateSeekerDto {
+    @IsString()
+    @ApiProperty({
+    description: 'seeker\'s skills',
+    example: 'DevOps',
+  })
+  skills: string;
+
+  @IsString()
+  @ApiProperty({
+      description: 'seeker\'s experience',
+      example: '2 years as an Frontend developper',
+  })
+  experience: string;
+  
+  @IsString()
+  @ApiProperty({
+      description: 'seeker\'s availability',
+      example: '12/02/2027 - 18/12/2027',
+  })
+  availability: string;
+}
