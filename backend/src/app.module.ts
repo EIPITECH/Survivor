bc09bfa8673709340cb99f7a2eb6d8157977be5c
hop3calypse <hugo.hamet@proton.me>
2026-09-07 11:20:15 +0200

    feat(seeder): Launch seeder in app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,9 +11,9 @@ import { Job } from './jobs/entities/job.entity';
 import { HealthModule } from './health/health.module';
 import { TilesModule } from './tiles/tiles.module';
 import configuration from './config/configuration';
-import { AdminSeederService } from './seeders';
 import { SeekersModule } from './users/seekers/seekers.module';
 import { Seeker } from './users/seekers/entities/seeker.entity';
+import { SeederService } from './seeders';
 
 @Module({
   imports: [
@@ -44,7 +44,7 @@ import { Seeker } from './users/seekers/entities/seeker.entity';
     SeekersModule
   ],
   controllers: [AppController],
-  providers: [AppService, AdminSeederService],
+  providers: [AppService, SeederService],
 })
 
 export class AppModule {}
