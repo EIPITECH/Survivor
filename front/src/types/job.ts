5080915b61613659f6378a039f6cde11a0218fcb
eiffis <enzo.iffis@epitech.eu>
2026-09-08 01:51:29 +0200

    feat(frontend/map): add types/jobs.ts to declarate the two differents type cityGroup and job, to avoid type conflicts, add cityJobPanel for the menu which appears when an user click on the cityMarker

---
+++ front/src/types/job.ts
@@ -0,0 +1,26 @@
+export interface Job {
+    id: number;
+    title: string;
+    description: string;
+    cityName: string;
+
+    latitude: number;
+    longitude: number;
+
+    employerId: number;
+    status: string;
+    companyName: string;
+    createdAt: string;
+
+    streetNumber?: number | null;
+    streetName?: string | null;
+    zipCode?: number | null;
+}
+
+export interface CityGroup {
+    cityName: string;
+    latitude: number;
+    longitude: number;
+    count: number;
+    jobs: Job[];
+}
