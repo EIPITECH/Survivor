083eb9d45cddbcb00201bf5651f705b2508565d1
eiffis <enzo.iffis@epitech.eu>
2026-09-07 11:19:16 +0200

    feat(backend/resource): import Application module in both module

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -4,9 +4,10 @@ import { JobsController } from './jobs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from './entities/job.entity';
 import { AuthModule } from '../auth/auth.module';
+import { Application } from '../users/application/entities/application.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Job]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Job, Application]), AuthModule],
   controllers: [JobsController],
   providers: [JobsService],
 })
