4b8014e73ee7852d878c678b5cc42a063c9e341a
eiffis <enzo.iffis@epitech.eu>
2026-09-08 21:28:05 +0200

    feat(backend/auth): new version of validate function which will check if the account of the user is suspended, if it is, he can't login, same in validate user

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,11 +1,15 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { jwtConstants } from './constants';
+import { UsersService } from '../users/users.service';
+import { UserAccountStatus } from '../users/enum/user-account-status.enum';
+import { User } from '../users/entities/user.entity';
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,6 +18,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
+    const user = await this.usersService.findOne(payload.sub);
+    
+    if (!user) {
+      throw new UnauthorizedException("Utilisateur introuvable");
+    }
+    if (user.accountStatus === UserAccountStatus.SUSPENDED) {
+      throw new UnauthorizedException("Votre compte a été suspendu");
+    }
+    
     return { firstName: payload.firstName, userId: payload.sub, email: payload.email, role: payload.role};
   }
 }
