e702e180d05697c1058d63399864a8c0c8338ac4
eiffis <enzo.iffis@epitech.eu>
2026-09-10 09:37:42 +0200

    fix(backend/main): fix enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4321'
+    origin: '*'
   });
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port') ?? 3000;
